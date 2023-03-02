7a7a90ddc02c6fc4f095271059eff3fc8fa9443b
CoderFrancis <mfcollegeacc@gmail.com>
2023-03-02 07:00:36 -0500

    Inheritance Submission Assignment

---
+++ Warrior_Class.py
@@ -0,0 +1,24 @@
+#this is the category of the players the user gets to pick from.
+#They can either choose a warrior or stealth class.
+
+#the information is waiting for user input.
+class Player:
+    name = ' '
+    age = ' '
+    height = ' '
+    weight = ' '
+
+#the description and attributes of the warrior class.
+class Warrior:
+    weapon = long_sword
+    dmg = 100
+    fighting_stlye = 'Brass, loud, flashy. Prefers close combat.'
+
+#the description and attributes of the stealth class.
+class Stealth:
+    weapon = daggers
+    dmg = 10
+    fighting_stlye = 'Silent. Prefers catching their enemy off guard.'
+    
+    
+    
