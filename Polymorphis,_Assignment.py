d63c648e737a3a78e16e51c7ff7950bbf0978ed1
CoderFrancis <mfcollegeacc@gmail.com>
2023-03-02 21:30:07 -0500

    Update Polymorphis,_Assignment.py

--- Polymorphis,_Assignment.py
+++ Polymorphis,_Assignment.py
@@ -1,42 +1,24 @@
 #Parent Class User
-class User:
-    name = "Mark"
-    email = "mark@gmail.com"
-    password = "1234abcd"
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
 
-    def getLoginInfo(self):
-        entry_name = input("Enter your name: ")
-        entry_email = input("Enter your email: ")
-        entry_password = input("Enter your password: ")
-        if (entry_email == self.email and entry_password == self.password):
-            print("Welcome back, {}!".format(entry_name))
-        else:
-            print("The password or email is incorrect.")
+    def printname(self):
+        print(self.firstname, self.lastname)
 
+x = Person("John", "Doe")
+x.printname()
 
-#Child Class Employee
-class Employee(User):
-    base_pay = 11.00
-    department = "General"
-    pin_number = "3980"
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+        print(self.graduationyear)
 
-#This is the same method in the parent class "User".
-#We've swapped entry_password with entry_pin.
+x = Student("Jane", "Doe", 2023)
+x.printname()
 
 
-    def getLoginInfo(self):
-        entry_name = input("Enter your name: ")
-        entry_email = input("Enter your email: ")
-        entry_pin = input("Enter your pin: ")
-        if (entry_email == self.email and entry_pin == self.pin_number):
-            print("Welcome back, {}!".format(entry_name))
-        else:
-            print("The password or email is incorrect.")
-
-#The following code invokes the methods inside each class for User and Employee.
-            
-customer = User()
-customer.getLoginInfo()
-
-manager = Employee()
-manager.getLoginInfo()
+        
+        
